c034baed62eccde9f4391ad083467361300d9045
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-03-26 21:16:17 -0400

    Add files via upload

--- Assignment2/Loader.java
+++ Assignment2/Loader.java
@@ -1,69 +1,88 @@
 package Assignment2;
 
-import Assignment1.Business;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
 
-import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class Loader {
-    @Test
+
+    String label1;
+    String label2;
+
     public void WriteWithFileChannelUsingRandomAccessFile()
             throws IOException {
         JSONParser parser = new JSONParser();
         JSONArray jsonArray = null;
         try {
-            jsonArray = (JSONArray) parser.parse(new FileReader("src\\Assignment2\\file1.json"));
+            jsonArray = (JSONArray) parser.parse(new FileReader("src\\Assignment2\\dataset.json"));
         } catch (IOException | ParseException ex) {
             ex.printStackTrace();
         }
 
         Gson gson = new Gson();
+        ExtendibleHashTable hashtable = new ExtendibleHashTable();
 
 
-        String str = null;
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
 
-            for (Object obj : jsonArray) {
-                JSONObject jsonObject = (JSONObject) obj;
+            Assignment2.Business business = gson.fromJson(jsonObject.toJSONString(), Assignment2.Business.class);
+            String str = business.categories;
 
-                Assignment2.Business business = gson.fromJson(jsonObject.toJSONString(), Assignment2.Business.class);
-                str = business.categories;
+            String file = "src/Assignment2/data/" + business.business_id + ".txt";
+            FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
 
-                String file = "src/Assignment2/data/" + business.name + ".txt";
-                FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
+            hashtable.put(business.name, business.business_id + ".txt");
 
-                assert str != null;
-                byte[] bytes = str.getBytes();
-                ByteBuffer set1bb = ByteBuffer.wrap(bytes);
-                channel.write(set1bb);
-                channel.close();
-                set1bb.clear();
+            assert str != null;
+            byte[] bytes = str.getBytes();
+            ByteBuffer set1bb = ByteBuffer.wrap(bytes);
+            channel.write(set1bb);
+            channel.close();
+            set1bb.clear();
+        }
+        String input = Application.input;
+        RandomAccessFile reader = new RandomAccessFile("src/Assignment2/data/" + hashtable.get(input), "r");
+        String inputCategory = reader.readLine();
+        reader.close();
+        List<String> catList = List.of(inputCategory.split(", "));
+        System.out.println(catList);
+        List<List<String>> docs = new ArrayList<>();
+        docs.add(catList);
+
+        int position = 0;
+        double check = 0;
+        tfIdfCalculator calculator = new tfIdfCalculator();
+        double currentTfIdf = 0.0;
+
+        List<String> clusters = Arrays.asList("Italian", "Pizza", "American (Traditional)", "Mexican", "Chinese");
+        for (int i = 0; i < docs.size(); i++) {
+            for (int j = 0; j < clusters.size(); j++) {
+                currentTfIdf = calculator.tfIdf(docs.get(i), Application.documents, clusters.get(j));
+                System.out.println(currentTfIdf);
+                if (currentTfIdf > check) {
+                    position = j;
+                    check = currentTfIdf;
+                }
             }
-/*
-            // verify
-            RandomAccessFile reader = new RandomAccessFile(file, "r");
-            System.out.println(reader.readLine());
-            reader.close();
-        */
+        }
+
+
+        label1 = clusters.get(position);
+        label2 = clusters.get(position);
+
     }
 }
